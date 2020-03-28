a92a8229ab349113f9f99dc1344c456956c5c842
Alexandre <alehsilva@live.com>
2020-03-28 16:39:55 -0300

    Projeto iniciado mostrando mensagem Ola Mundo

---
+++ TreinoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreinoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
